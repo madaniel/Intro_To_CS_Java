9c46c3760f97569230576aff723a6920cb5da19b
Daniel Madar <m.a.daniel@gmail.com>
2019-06-07 20:33:06 +0300

    - add number ready - length ready

--- src/main/maman_15/IntListTwo.java
+++ src/main/maman_15/IntListTwo.java
@@ -39,7 +39,47 @@ public class IntListTwo {
      * @param num number to add
      */
     public void addNumber(int num){
+        IntNodeTwo newNum = new IntNodeTwo(num);
+        IntNodeTwo pointer;
 
+        // List is empty
+        if(isListEmpty()){
+            this._head = newNum;
+            this._tail = newNum;
+            return;
+        }
+
+        // List is not empty
+        for(pointer = this._head; pointer.getNext() != null && pointer.getNum() < num ; pointer = pointer.getNext());
+
+        // newNum should be first
+        if(isSingle(pointer) && num <= pointer.getNum()){
+            newNum.setNext(pointer);
+            pointer.setPrev(newNum);
+            this._head = newNum;
+            this._tail = pointer;
+        }
+
+        // newNum should be second
+        else if(isSingle(pointer) && num > pointer.getNum()){
+            newNum.setPrev(pointer);
+            pointer.setNext(newNum);
+            this._tail = newNum;
+        }
+
+        // newNum should be last
+        else if(isLast(pointer) && num > pointer.getNum()){
+            newNum.setPrev(newNum);
+            pointer.setNext(newNum);
+            this._tail = pointer;
+        }
+
+        // newNum should be in between
+        else {
+            IntNodeTwo prev = pointer.getPrev();
+            pointer.setPrev(newNum);
+            prev.setNext(newNum);
+        }
     }
 
     /**
@@ -51,12 +91,14 @@ public class IntListTwo {
         IntNodeTwo pointer;
         for(pointer = this._head; pointer != null && pointer.getNum() != num ; pointer = pointer.getNext());
 
-        // Either List is empty or number not found
+        // Either List is empty or number not found - do nothing
         if(pointer == null);
 
-        // Number is single in list, we detach the head
-        else if(this.isSingle(pointer))
+        // Number is single in list, we detach the head and tail
+        else if(this.isSingle(pointer)){
             this._head = null;
+            this._tail = null;
+        }
 
         // Number is first in list, head will point 2nd number and its previous will detach
         else if(this.isFirst(pointer)){
@@ -69,6 +111,7 @@ public class IntListTwo {
         else if(this.isLast(pointer)){
             IntNodeTwo prev = pointer.getPrev();
             prev.setNext(null);
+            this._tail = prev;
         }
 
         // Number is in the middle, we attach previous node to next one and next one to previous
@@ -89,7 +132,15 @@ public class IntListTwo {
     }
 
     public String toString(){
-        return "";
+        IntNodeTwo pointer;
+        String listString = "{";
+
+        for(pointer = this._head; pointer != null; pointer = pointer.getNext())
+            listString += pointer.getNum() + ", ";
+
+        listString += "}";
+
+        return listString;
     }
 
     /**
@@ -97,7 +148,13 @@ public class IntListTwo {
      * @return number of numbers in the list
      */
     public int length(){
-        return 0;
+        IntNodeTwo pointer;
+        int counter = 0;
+
+        for(pointer = this._head; pointer != null; pointer = pointer.getNext())
+            counter++;
+
+        return counter;
     }
 
     /**
@@ -105,8 +162,13 @@ public class IntListTwo {
      * @return sum of the numbers in the list
      */
     public int sum(){
-        return 0;
+        IntNodeTwo pointer;
+        int sum = 0;
 
+        for(pointer = this._head; pointer != null; pointer = pointer.getNext())
+            sum += pointer.getNum();
+
+        return sum;
     }
 
     /**
@@ -114,7 +176,39 @@ public class IntListTwo {
      * @return length of the longest sub-array even sum
      */
     public int maxLength(){
-        return 0;
+        if(isListEmpty())
+            return 0;
+
+        IntNodeTwo leftPointer=_head, rightPointer=_tail;
+        int sum = sum();
+        int count = length();
+
+        // Base case - sum is even
+        if (sum % 2 == 0)
+            return count;
+
+        // Sum is odd + odd number from the right
+        // Iterate from the right, decreasing value until the sum is even
+        int tmpSum = sum, rightLength = count;
+
+        while(tmpSum % 2 != 0){
+            tmpSum -= rightPointer.getNum();
+            rightLength --;
+            rightPointer = rightPointer.getPrev();
+        }
+
+        // Sum is odd + odd number from the left
+        // Iterate from the left, decreasing value until the sum is even
+        int leftLength = count;
+        tmpSum = sum;
+
+        while(tmpSum % 2 != 0){
+            tmpSum -= leftPointer.getNum();
+            leftPointer = leftPointer.getNext();
+            leftLength --;
+        }
+
+        return Math.max(leftLength, rightLength);
     }
 
     /**
@@ -123,19 +217,45 @@ public class IntListTwo {
      * @return true if there's sub-array with average num, false otherwise
      */
     public boolean isAverage(double num){
-        return false;
+        if(isListEmpty())
+            return false;
+
+        IntNodeTwo leftPointer=_head, rightPointer=_tail;
+        int sum = sum();
+        int count = length();
+        double average = sum /(double)count;
+
+        // Increasing or decreasing the average to find the target num
+        while(leftPointer != rightPointer || num != average){
+
+            if(average < num){
+                sum -= leftPointer.getNum();
+                count -= 1;
+                average = sum /(double)count;
+                leftPointer = leftPointer.getNext();
+            }
+
+            else if(average > num){
+                sum -= rightPointer.getNum();
+                count -= 1;
+                average = sum /(double)count;
+                rightPointer = rightPointer.getPrev();
+            }
+        }
+
+        return num == average;
     }
 
     private boolean isSingle(IntNodeTwo nodePointer){
-        return this._head == nodePointer && nodePointer.getNext() == null;
+        return this._head == nodePointer && this._tail == nodePointer;
     }
 
     private boolean isFirst(IntNodeTwo nodePointer){
-        return this._head == nodePointer && nodePointer.getNext() != null;
+        return this._head == nodePointer && this._tail != nodePointer;
     }
 
     private boolean isLast(IntNodeTwo nodePointer){
-        return nodePointer.getNext() == null && nodePointer.getPrev() != null;
+        return this._head != nodePointer && this._tail == nodePointer;
     }
 
     private boolean isInBetween(IntNodeTwo nodePointer){
@@ -143,7 +263,6 @@ public class IntListTwo {
     }
 
     private boolean isListEmpty(){
-        return this._head == null;
+        return this._head == null && this._tail == null;
     }
-
 }
